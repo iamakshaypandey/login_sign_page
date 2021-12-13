da553f1bf209116d0e701961087be5eec9f550ec
Akshay pandey <85728247+iamakshaypandey@users.noreply.github.com>
2021-12-13 14:10:35 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from login import *
 from signup import *
 print('1. signup 2. login  :- ')
-inp=int(input("selact option:- 1. or 2."))
+inp=int(input("select option:- 1. or 2."))
 if inp==1:
     js_file() 
 else:
